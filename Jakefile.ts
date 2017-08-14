c0de323418098d5fb3bea07e59db203bc3a3b59d
Mehrdad Reshadi <mreshadi@instartlogic.com>
2017-08-13 23:56:56 -0700

    make sure the test task in a private namespace

--- Jakefile.ts
+++ Jakefile.ts
@@ -5,37 +5,39 @@ import * as Tsc from "jakets/lib/TscTask";
 import { RollupTask } from "./lib/RollupTask";
 let MakeRelative = Jakets.CreateMakeRelative(__dirname);
 
-Jakets.GlobalTask(
-  "test_rollup"
-  , [
-    RollupTask(
-      "rollup"
-      , Jakets.BuildDir + "/compile/all.js"
-      , [Jakets.BuildDir + "/compile/Main.js"]
-      , [
-        Tsc.TscTask(
-          "tsc"
-          , [MakeRelative("./tests/Main.ts")]
-          , []
-          , {
-            outDir: "build/compile",
-            module: Tsc.ModuleKind.ES2015,
-            target: Tsc.ScriptTarget.ES5
+namespace("rollup", () => {
+  Jakets.GlobalTask(
+    "test"
+    , [
+      RollupTask(
+        "rollup"
+        , Jakets.BuildDir + "/compile/all.js"
+        , [Jakets.BuildDir + "/compile/Main.js"]
+        , [
+          Tsc.TscTask(
+            "tsc"
+            , [MakeRelative("./tests/Main.ts")]
+            , []
+            , {
+              outDir: "build/compile",
+              module: Tsc.ModuleKind.ES2015,
+              target: Tsc.ScriptTarget.ES5
+            }
+          )
+        ]
+        , {
+          Bundle: {
+            dest: "",
+            format: "iife",
+            moduleName: "TopModule",
+            sourceMap: "inline",
+            useStrict: false
           }
-        )
-      ]
-      , {
-        Bundle: {
-          dest: "",
-          format: "iife",
-          moduleName: "TopModule",
-          sourceMap: "inline",
-          useStrict: false
         }
-      }
-    )
-  ]
-  , async () => {
-    require("./" + Jakets.BuildDir + "/compile/all");
-  }
-);
+      )
+    ]
+    , async () => {
+      require("./" + Jakets.BuildDir + "/compile/all");
+    }
+  );
+});
